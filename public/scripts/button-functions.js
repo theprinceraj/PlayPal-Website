8cd6a5d7c01959cbcab60b2953ce33b1eb62ffef
Prince Raj <profile.princeraj+github@gmail.com>
2023-11-16 10:30:07 +0530

    .

--- public/scripts/button-functions.js
+++ public/scripts/button-functions.js
@@ -5,3 +5,10 @@ messageBtn.addEventListener('click', () => {
     // Direct to the given link
     window.location.href = "https://discord.com/users/564327207133249536";
 });
+
+const searchButton = document.querySelector('.search__icon');
+const searchInput = document.querySelector('.search__field');
+searchButton.addEventListener('click', () => {
+    const inputValue = searchInput.value;
+    alert(inputValue);
+});

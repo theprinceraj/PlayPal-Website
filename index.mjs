7c9fbf1ddf5131d2292969665dcbc7db6dfe9f23
Prince Raj <profile.princeraj+github@gmail.com>
2023-11-17 20:57:20 +0530

    .

---
+++ index.mjs
@@ -0,0 +1,38 @@
+import express, { static as expressStatic } from 'express';
+import { displayProfileCard } from './public/utilities/displayProfileCard.mjs';
+
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express();
+const port = process.env.PORT || 3000; // Use the provided port or default to 3000
+
+// Serve static files from the 'public' directory
+app.use(expressStatic(join(__dirname, '/public')));
+
+// Define routes for specific files
+app.get('/', (req, res) => {
+    res.sendFile(join(__dirname, '/public', 'index.html'));
+});
+
+app.use('/search', async (req, res) => {
+    try {
+        const userId = req.query.userId;
+        let markup = await displayProfileCard(userId.toString());
+        res.setHeader('Content-Type', 'text/html');
+        res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+        res.end(markup);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Error generating profile card');
+    }
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
+
+export default app;
